f841fe7078cedef6c17b58b31da7221510f52f91
Ramin Keshavarzi <raminkeshavarzi@organiplace.com>
2021-06-07 07:01:21 +0200

    Activate read more link by making request

--- Tech-News/Tech-News/View/DetailView.swift
+++ Tech-News/Tech-News/View/DetailView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import WebKit
 
 struct DetailView: View {
     
@@ -13,7 +14,7 @@ struct DetailView: View {
     
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        WebView(urlString: url)
     }
 }
 
@@ -21,4 +22,23 @@ struct DetailView_Previews: PreviewProvider {
     static var previews: some View {
         DetailView(url: "https://www.apple.com")
     }
+}
+
+struct WebView: UIViewRepresentable {
+    
+    let urlString: String?
+    
+    func makeUIView(context: Context) -> WebView.UIViewType {
+        return WKWebView()
+    }
+    
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        if let safeString = urlString {
+            if let url = URL(string: safeString) {
+                let request = URLRequest(url: url)
+                uiView.load(request)
+            }
+        }
+    }
+    
 }
